2c95dccfc04610c2bafdf4a5374e94e5b4312ecd
Alex Griessman <alex.griessman@gmail.com>
2015-12-12 23:45:41 -0500

    Merged "Now using the advection-diffusion equation for mass transfer."

--- slab-drying/mass/diffusion.h
+++ slab-drying/mass/diffusion.h
@@ -7,8 +7,8 @@
 struct fe1d;
 
 double ResMass(struct fe1d *, matrix *, Elem1D *, double, int, int);
-double ResMass_dCdu(struct fe1d *, matrix *, Elem1D *, double, int, int);
 double ResDtMass(struct fe1d *, matrix *, Elem1D *, double, int, int);
+double ResDtMass_Du(struct fe1d *, matrix *, Elem1D *, double, int, int);
 double ConvBCMass(struct fe1d *, int);
 double ExternalConc(struct fe1d *, int);
 
